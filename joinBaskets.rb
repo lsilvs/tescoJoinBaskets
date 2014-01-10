21c934c78f3499e5f4ce775837c36bc9f7b3178a
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-01-10 14:36:17 +0000

    Implement the ruby script to do such task

---
+++ joinBaskets.rb
@@ -0,0 +1,75 @@
+require 'rubygems'
+require 'highline/import'
+require 'nokogiri'
+require 'mechanize'
+require 'open-uri'
+
+# @author Lucas Silvestre (https://github.com/lsilvs/)
+class JoinBaskets
+	# Define some vars
+  @url_login = "https://secure.tesco.ie/register/default.aspx?from=http%3a%2f%2fwww.tesco.ie%2fgroceries%2fbasket%2fDefault.aspx"
+	@shelf = []
+
+	agent = Mechanize.new
+
+  agent.get(@url_login)
+  form = agent.page.forms.first
+
+  # Ask user for the credentials
+  form.loginID = ask("Enter your Email address:  ") { |q| q.echo = true }
+  form.password = ask("Enter your password:  ") { |q| q.echo = "*" }
+
+  form.submit
+
+  if agent.page.title == "Tesco.ie - Sorry, but we couldn't sign you in"
+    p agent.page.title
+  else
+    p agent.page.title
+    # Access the main basket (My Basket)
+    agent.get("http://www.tesco.ie/groceries/basket/")
+    form = agent.page.form_with(:id => "fBasket")
+    form.field_with(:name => 'baskets').options.first.click
+    form.click_button
+
+    # Empty the main basket before join the others
+    agent.get("http://www.tesco.ie/groceries/dialogue/?dialogueName=EmptyBasket")
+    agent.submit(agent.page.forms.first, agent.page.forms.first.button_with(:name => 'emptyBasket'))
+
+    # Iterate through each basket
+    form.field_with(:name => 'baskets').options.each do |opt|
+      opt.click
+      form.click_button
+
+      # Iterate through each product on shelfs
+      agent.page.parser.css("tbody.shelf").each do |row|
+        if row.css("p.prodName a").any? and row.css("input.basketItemQuantity").any?
+            product = { 
+            "name" => row.css("p.prodName a").text, 
+            "link" => row.css("p.prodName a")[0]["href"],
+            "quantity" => row.css("input.basketItemQuantity")[0]["value"],
+          }
+          @shelf.push(product)
+        end
+      end
+    end
+
+    # Access the main basket (My Basket)
+    agent.get("http://www.tesco.ie/groceries/basket/")
+    form = agent.page.form_with(:id => "fBasket")
+    form.field_with(:name => 'baskets').options.first.click
+    form.click_button
+
+    # Add each product to the main basket
+    @shelf.each do |product|
+       agent.get(product['link'])
+       form = agent.page.form_with(:id => %r{fDetails})
+       form.fields[2].value = product['quantity']
+       agent.submit(form, form.buttons[0])
+    end
+
+  p "All your baskets were joined together into the 'My Basket'. Check it out on Tesco.ie ;)"
+  p "Cheers for use me and spread the word!"
+
+  end
+
+end
